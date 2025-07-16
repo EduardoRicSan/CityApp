2776725f559c4c59ece1809858923b7421ef984e
Lalo <exiga@MacBook-Pro-de-Lalo.local>
2025-07-16 11:27:06 -0600

    Last clean of code. Test done

--- app/src/main/java/com/tablegroup/ualaapptest/ui/composables/CityListScreen.kt
+++ app/src/main/java/com/tablegroup/ualaapptest/ui/composables/CityListScreen.kt
@@ -4,17 +4,14 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -175,7 +172,6 @@ fun CityRow(
                 Modifier
                     .weight(1f)
                     .fillMaxHeight()
-                   // .clickable { onCityClicked(city) }
             ) {
                 Text(text = "${city.name}, ${city.country}", style = MaterialTheme.typography.bodySmall)
                 Text(text = "Lat: ${city.lat}, Lon: ${city.lon}", style = MaterialTheme.typography.bodyMedium)
--- domain/src/main/java/com/tablegroup/domain/repository/CityRepository.kt
+++ domain/src/main/java/com/tablegroup/domain/repository/CityRepository.kt
@@ -13,7 +13,6 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 /**
